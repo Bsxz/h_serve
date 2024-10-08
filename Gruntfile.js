8f8f9fb3d98c1756f90b2af8d4a136f2f03b83e3
baiye <baiyeqb@163.com>
2024-10-08 08:37:07 +0800

    ff

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,65 @@
-/**
- * Gruntfile
- *
- * This Node script is executed when you run `grunt`-- and also when
- * you run `sails lift` (provided the grunt hook is installed and
- * hasn't been disabled).
- *
- * WARNING:
- * Unless you know what you're doing, you shouldn't change this file.
- * Check out the `tasks/` directory instead.
- *
- * For more information see:
- *   https://sailsjs.com/anatomy/Gruntfile.js
- */
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+  // 配置任务
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
 
-  var loadGruntTasks = require('sails-hook-grunt/accessible/load-grunt-tasks');
+    // 清理 dist 文件夹
+    clean: {
+      build: ["dist"],
+    },
 
-  // Load Grunt task configurations (from `tasks/config/`) and Grunt
-  // task registrations (from `tasks/register/`).
-  loadGruntTasks(__dirname, grunt);
+    // 复制文件到 dist 文件夹
+    copy: {
+      build: {
+        files: [{ expand: true, cwd: "src/", src: ["**"], dest: "dist/" }],
+      },
+    },
 
+    // 压缩 JS 文件
+    uglify: {
+      build: {
+        files: {
+          "dist/js/app.min.js": ["src/js/app.js"],
+        },
+      },
+    },
+
+    // 压缩 CSS 文件
+    cssmin: {
+      build: {
+        files: {
+          "dist/css/style.min.css": ["src/css/style.css"],
+        },
+      },
+    },
+
+    // 压缩 HTML 文件
+    htmlmin: {
+      build: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true,
+        },
+        files: {
+          "dist/index.html": "src/index.html",
+        },
+      },
+    },
+  });
+
+  // 加载任务插件
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-htmlmin");
+
+  // 定义默认任务
+  grunt.registerTask("default", [
+    "clean",
+    "copy",
+    "uglify",
+    "cssmin",
+    "htmlmin",
+  ]);
 };
